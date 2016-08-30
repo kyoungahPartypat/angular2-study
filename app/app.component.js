ffef16d1c03a929d29f270a8799a66b2fb841dcd
kyoungah <kyoungah@kyoungah.com>
2016-08-31 01:50:59 +0900

    chore(*) : 리스트에 클릭 이벤트 추가

--- app/app.component.js
+++ app/app.component.js
@@ -16,26 +16,29 @@ var Twice = (function () {
 }());
 exports.Twice = Twice;
 var TWICE = [
-    { id: 1, name: '나연', age: 22, nick: "나부기" },
-    { id: 2, name: '정연', age: 21, nick: "유또막" },
-    { id: 3, name: '지효', age: 20, nick: "갓지효" },
-    { id: 4, name: '다현', age: 19, nick: "두부" },
-    { id: 5, name: '채영', age: 18, nick: "챙" },
-    { id: 6, name: '모모', age: 21, nick: "모모링" },
-    { id: 7, name: '사나', age: 21, nick: "사또떨" },
-    { id: 8, name: '미나', age: 20, nick: "정남이" },
-    { id: 9, name: '쯔위', age: 18, nick: "요다" }
+    { id: 1, name: '나연', age: 22, nick: "나부기", image: "./public/image/naYeon.jpg" },
+    { id: 2, name: '정연', age: 21, nick: "유또막", image: "./public/image/jeongYeon.jpg" },
+    { id: 3, name: '지효', age: 20, nick: "갓지효", image: "./public/image/juHyo.jpg" },
+    { id: 4, name: '다현', age: 19, nick: "두부", image: "./public/image/daHyun.jpg" },
+    { id: 5, name: '채영', age: 18, nick: "챙", image: "./public/image/chaeYoung.jpg" },
+    { id: 6, name: '모모', age: 21, nick: "모모링", image: "./public/image/momo.jpg" },
+    { id: 7, name: '사나', age: 21, nick: "사또떨", image: "./public/image/sana.jpg" },
+    { id: 8, name: '미나', age: 20, nick: "정남이", image: "./public/image/mina.jpg" },
+    { id: 9, name: '쯔위', age: 18, nick: "요다", image: "./public/image/tzuYu.jpg" }
 ];
 var AppComponent = (function () {
     function AppComponent() {
-        this.title = "ONE IN A MILLION!";
+        this.title = "ONE IN A MILLION! TWICE!";
         this.twice = TWICE;
     }
+    AppComponent.prototype.onSelect = function (member) {
+        this.selectedMember = member;
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'twice',
-            styles: ["\n      h1, h2{text-align:center}\n\n      #content{width:150px; margin-left:auto; margin-right:auto}\n      #content > ul.members{width:100%}\n      #content > ul.members > li{margin-bottom:10px; line-height:22px}\n\n    "],
-            template: "\n      <h1>{{title}}</h1>\n      <h2>Twice!</h2>\n      <div id=\"content\">\n        <ul class=\"members\">\n          <li *ngFor='let member of twice'>\n            <span class=\"member-title\">{{member.id}}. <span class={{member.name}}</span>\n            <div>\n              <span>\uB098\uC774: {{member.age}}</span>\n              <br/>\n              <span>\uBCC4\uBA85: {{member.nick}}</span>\n            </div>\n          </li>\n        </ul>\n      </div>\n  "
+            styles: ["\n      h1{text-align:center; margin-top:10px; margin-bottom:30px; color:#42d8cd}\n\n      #content{width:450px; margin-left:auto; margin-right:auto}\n      #content:after{content:\" \"; display:block; clear:both}\n\n      #content > ul.members{width:120px; float:left}\n      #content > ul.members > li{width:100%; margin-bottom:10px; line-height:22px}\n      #content > ul.members > li span{display:inline-block}\n      #content > ul.members > li > span{width:100%; height:35px; border-radius:20px; border:1px solid #42d8cd}\n      #content > ul.members > li > span:hover{border:1px solid #cacaca; background-color:#cacaca; color:#FFF}\n      #content > ul.members > li > span > span.member-number{width:30px; height:30px; margin-top:3px; margin-left:5px; margin-right:5px; line-height:30px; text-align:center; font-size:13px; color:#FFF; border-radius:100%; background-color:#42d8cd}\n\n      #content > ul.members > li > span.selected{border:1px solid #cacaca; background-color:#cacaca; color:#FFF}\n      #content > ul.members > li > span.selected > span.member-number{color:#cacaca; background-color:#FFF}\n\n      #content > div.detail{width:300px; float:left; margin-left:30px; text-align:center}\n      #content > div.detail span{font-size:16px; color:#bdbfc3}\n\n      #content > div.detail > strong{font-size:20px; color:#bdbfc3}\n\n      #content > div.detail > img{width:100%; height:auto; margin-top:5px; margin-bottom:10px; border:2px solid #42d8cd}\n\n      #content > div.detail > span.member-nick > input{height:25px; padding-left:10px; color:#bdbfc3; border:1px solid #bdbfc3; border-radius:5px}\n    "],
+            template: "\n      <h1>{{title}}</h1>\n      <div id=\"content\">\n        <ul class=\"members\">\n          <li *ngFor=\"let member of twice\" (click)=\"onSelect(member)\">\n            <span [class.selected]=\"member === selectedMember\"><span class=\"member-number\">{{member.id}}</span> {{member.name}}</span>\n          </li>\n        </ul>\n        <div class=\"detail\" *ngIf=\"selectedMember\">\n          <strong>{{selectedMember.name}}</strong>\n          <span class=\"member-age\">({{selectedMember.age}}\uC138)</span>\n          <img src={{selectedMember.image}}/>\n          <span class=\"member-nick\">\uBCC4\uBA85: <input [(ngModel)]=\"selectedMember.nick\" placeholder=\"name\"/></span>\n        </div>\n      </div>\n  "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -5,49 +5,72 @@ export class Twice {
   name: string;
   age: number;
   nick: string;
+  image: string;
 }
 
 const TWICE: Twice[] = [
-  {id: 1, name: '나연', age: 22, nick: "나부기"},
-  {id: 2, name: '정연', age: 21, nick: "유또막"},
-  {id: 3, name: '지효', age: 20, nick: "갓지효"},
-  {id: 4, name: '다현', age: 19, nick: "두부"},
-  {id: 5, name: '채영', age: 18, nick: "챙"},
-  {id: 6, name: '모모', age: 21, nick: "모모링"},
-  {id: 7, name: '사나', age: 21, nick: "사또떨"},
-  {id: 8, name: '미나', age: 20, nick: "정남이"},
-  {id: 9, name: '쯔위', age: 18, nick: "요다"}
+  {id: 1, name: '나연', age: 22, nick: "나부기", image: "./public/image/naYeon.jpg"},
+  {id: 2, name: '정연', age: 21, nick: "유또막", image: "./public/image/jeongYeon.jpg"},
+  {id: 3, name: '지효', age: 20, nick: "갓지효", image: "./public/image/juHyo.jpg"},
+  {id: 4, name: '다현', age: 19, nick: "두부", image: "./public/image/daHyun.jpg"},
+  {id: 5, name: '채영', age: 18, nick: "챙", image: "./public/image/chaeYoung.jpg"},
+  {id: 6, name: '모모', age: 21, nick: "모모링", image: "./public/image/momo.jpg"},
+  {id: 7, name: '사나', age: 21, nick: "사또떨", image: "./public/image/sana.jpg"},
+  {id: 8, name: '미나', age: 20, nick: "정남이", image: "./public/image/mina.jpg"},
+  {id: 9, name: '쯔위', age: 18, nick: "요다", image: "./public/image/tzuYu.jpg"}
 ];
 
 @Component({
   selector: 'twice',
   styles: [`
-      h1, h2{text-align:center}
+      h1{text-align:center; margin-top:10px; margin-bottom:30px; color:#42d8cd}
 
-      #content{width:150px; margin-left:auto; margin-right:auto}
-      #content > ul.members{width:100%}
-      #content > ul.members > li{margin-bottom:10px; line-height:22px}
+      #content{width:450px; margin-left:auto; margin-right:auto}
+      #content:after{content:" "; display:block; clear:both}
 
+      #content > ul.members{width:120px; float:left}
+      #content > ul.members > li{width:100%; margin-bottom:10px; line-height:22px}
+      #content > ul.members > li span{display:inline-block}
+      #content > ul.members > li > span{width:100%; height:35px; border-radius:20px; border:1px solid #42d8cd}
+      #content > ul.members > li > span:hover{border:1px solid #cacaca; background-color:#cacaca; color:#FFF}
+      #content > ul.members > li > span > span.member-number{width:30px; height:30px; margin-top:3px; margin-left:5px; margin-right:5px; line-height:30px; text-align:center; font-size:13px; color:#FFF; border-radius:100%; background-color:#42d8cd}
+
+      #content > ul.members > li > span.selected{border:1px solid #cacaca; background-color:#cacaca; color:#FFF}
+      #content > ul.members > li > span.selected > span.member-number{color:#cacaca; background-color:#FFF}
+
+      #content > div.detail{width:300px; float:left; margin-left:30px; text-align:center}
+      #content > div.detail span{font-size:16px; color:#bdbfc3}
+
+      #content > div.detail > strong{font-size:20px; color:#bdbfc3}
+
+      #content > div.detail > img{width:100%; height:auto; margin-top:5px; margin-bottom:10px; border:2px solid #42d8cd}
+
+      #content > div.detail > span.member-nick > input{height:25px; padding-left:10px; color:#bdbfc3; border:1px solid #bdbfc3; border-radius:5px}
     `],
   template: `
       <h1>{{title}}</h1>
-      <h2>Twice!</h2>
       <div id="content">
         <ul class="members">
-          <li *ngFor='let member of twice'>
-            <span class="member-title">{{member.id}}. <span class={{member.name}}</span>
-            <div>
-              <span>나이: {{member.age}}</span>
-              <br/>
-              <span>별명: {{member.nick}}</span>
-            </div>
+          <li *ngFor="let member of twice" (click)="onSelect(member)">
+            <span [class.selected]="member === selectedMember"><span class="member-number">{{member.id}}</span> {{member.name}}</span>
           </li>
         </ul>
+        <div class="detail" *ngIf="selectedMember">
+          <strong>{{selectedMember.name}}</strong>
+          <span class="member-age">({{selectedMember.age}}세)</span>
+          <img src={{selectedMember.image}}/>
+          <span class="member-nick">별명: <input [(ngModel)]="selectedMember.nick" placeholder="name"/></span>
+        </div>
       </div>
   `
 })
 
 export class AppComponent {
-  title = "ONE IN A MILLION!";
+  title = "ONE IN A MILLION! TWICE!";
   twice = TWICE;
+  selectedMember: Twice;
+
+  onSelect(member: Twice): void {
+    this.selectedMember = member;
+  }
 }
